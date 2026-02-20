c85ac5960ad70604dde8e0024d030c9407de043d
Flores41 <jose_flores28@usmp.pe>
2026-02-20 10:00:27 -0500

    Proyecto Angular completo con lista de tareas

---
+++ OneDrive/Documentos/uni/6 semestre/Angular/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Component1 }  from './component1/component1';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Component1],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Angular');
+}
